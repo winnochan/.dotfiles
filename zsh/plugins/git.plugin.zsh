148a456b5bc19ac741f71be5d9ea0b1093d842b6
WinnoChan <winnochan@163.com>
2024-03-18 02:24:30 +0800

    fix link error

--- zsh/plugins/git.plugin.zsh
+++ zsh/plugins/git.plugin.zsh
@@ -1,20 +1,20 @@
-if [ "$USER" = "winnochan" ]; then
-  if [ ! -L ~/.gitconfig ]; then
-    if [ -e ~/.gitconfig ]; then
-      rm -rf ~/.gitconfig
-    fi
-    ln -s $ZSH_DIR/config/gitconfig ~/.gitconfig
-    echo 'link git config'
-  fi
+# if [ "$USER" = "winnochan" ]; then
+#   if [ ! -L ~/.gitconfig ]; then
+#     if [ -e ~/.gitconfig ]; then
+#       rm -rf ~/.gitconfig
+#     fi
+#     ln -s $ZSH_DIR/config/gitconfig ~/.gitconfig
+#     echo 'link git config'
+#   fi
 
-  if [ ! -L ~/.gitattributes ]; then
-    if [ -e ~/.gitattributes ]; then
-      rm -rf ~/.gitattributes
-    fi
-    ln -s $ZSH_DIR/config/gitattributes ~/.gitattributes
-    echo 'link git attributes'
-  fi
-fi
+#   if [ ! -L ~/.gitattributes ]; then
+#     if [ -e ~/.gitattributes ]; then
+#       rm -rf ~/.gitattributes
+#     fi
+#     ln -s $ZSH_DIR/config/gitattributes ~/.gitattributes
+#     echo 'link git attributes'
+#   fi
+# fi
 
 # export GCM_CREDENTIAL_STORE=gpg
 export GCM_CREDENTIAL_STORE=secretservice
