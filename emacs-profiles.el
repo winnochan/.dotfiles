4dcc524b5161efb0c132537ad1bf6d232062e909
WinnoChan <winnochan@163.com>
2024-03-18 04:04:15 +0800

    add doomemacs config

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,10 +1,12 @@
 (
  ("default" . ((user-emacs-directory . "~/.spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+               (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
 
  ("spacemacs" . ((user-emacs-directory . "~/.spacemacs")
+                 (server-name . "spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
 
  ("doomemacs" . ((user-emacs-directory . "~/.doomemacs")
-                 (env . (("DOOMDIR" . "~/.doomemacs.d")))))
+                 (server-name . "doomemacs")
+                 (env . (("DOOMDIR" . "~/.doom.d")))))
  )
