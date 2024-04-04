ce857b7a02e9416fc0a85e3f2216d9436d380cdc
WinnoChan <winnochan@163.com>
2024-04-04 23:37:37 +0800

    update asdf config

--- zsh/plugins/asdf.plugin.zsh
+++ zsh/plugins/asdf.plugin.zsh
@@ -1,7 +1,27 @@
+# for golang
+export ASDF_GOLANG_DEFAULT_PACKAGES_FILE=$DOT_DIR/config/asdf/default-golang-pkgs
+__env_file=~/.asdf/plugins/golang/set-env.zsh
+if [ -f ${__env_file} ]; then
+    source ${__env_file}
+fi
+
+# for java
+__env_file=~/.asdf/plugins/java/set-java-home.zsh
+if [ -f ${__env_file} ]; then
+    source ${__env_file}
+fi
+
+# for nodejs
 export NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
 export ASDF_NODEJS_NODEBUILD_HOME=$DOT_DIR/3rd/node-build
 export ASDF_NPM_DEFAULT_PACKAGES_FILE=$DOT_DIR/config/asdf/default-npm-packages
 
+# for python
+export ASDF_PYTHON_DEFAULT_PACKAGES_FILE=$DOT_DIR/config/asdf/default-python-packages
+
+# for asdf
 source $DOT_DIR/3rd/asdf/asdf.sh
 # source $ZSH_DIR/.asdf/completions/asdf.bash
 fpath=(${ASDF_DIR}/completions $fpath)
+
+unset __env_file
