3619d64c929a5e0005cb2a3da64e3786ed3e721b
WinnoChan <winnochan@163.com>
2024-04-19 16:48:43 +0800

    add markdown layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -252,8 +252,8 @@ This function should only modify configuration layer settings."
            )
      ;; kotlin
      ;; (lua)
-     ;; (markdown :variables
-     ;;           markdown-live-preview-engine 'eww)
+     (markdown :variables
+               markdown-live-preview-engine 'eww)
      ;; (plantuml :variables
      ;;           plantuml-default-exec-mode 'jar
      ;;           plantuml-jar-path "~/.spacemacs.d/3rd/plantuml.jar"
